20094de96635ac0d9d51b261912b387b0c95d083
Tadej Murovec <tadej.murovec@gmail.com>
2010-06-07 18:23:36 +0200

    schedule error fix, pagetitle

--- src/helpers/application_helper.rb
+++ src/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
   
   def page_title
+    return @page_title unless @site_name
     full_page_title.join(" - ")
   end
   
   def full_page_title
-    page_title = [@site_name ||= "Tenis Bar Lokvica"]
+    page_title = [@site_name ||= nil]
     page_title << @page_title
   end
   
